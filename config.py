6852a01f7c66a0ae6734666d0db76f96985b36a1
Jessythopa <112460244+Jessythopa@users.noreply.github.com>
2024-06-05 21:44:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,9 @@ MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
     "mongodb+srv://moni:<password>@cluster0.i3djv5s.mongodb.net/?retryWrites=true&w=majority",
 )
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://graph.org/file/6e26583b2d4f1eed61ce1.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://graph.org/file/214f53702f788c668e294.jpg"
+)
